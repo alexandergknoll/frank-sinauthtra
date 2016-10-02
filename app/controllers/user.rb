b2f8d37d25bf5a29a5a5f9a5b9a3ead10f5a3184
Alex Knoll <alexandergknoll@gmail.com>
2016-10-02 16:11:56 -0500

    add edit/update routes to update profile info

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -21,3 +21,26 @@ post '/users' do
     end
   end
 end
+
+get '/users/:user_id/edit' do
+  if current_user && current_user.id.to_s == params[:user_id].to_s
+    @user = current_user
+    erb :'users/edit'
+  else
+    redirect '/'
+  end
+end
+
+post '/users/:user_id/update' do
+  if current_user && current_user.id.to_s == params[:user_id].to_s
+    @user = current_user
+    if @user.update(params[:user])
+      redirect "/"
+    else
+      @errors = @user.errors.full_messages
+      erb :'users/edit'
+    end
+  else
+    redirect '/'
+  end
+end
